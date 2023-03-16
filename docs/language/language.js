3988804a3416021685354fba2a72a0afe29b8746
olli <git add>
2023-03-16 20:47:29 +0800

    added two language support

---
+++ docs/language/language.js
@@ -0,0 +1,41 @@
+ const language = {
+
+    'chinese': {
+        "home": '首頁',
+        'english': "英文",
+        "chinese": "中文",
+        'onTheMove': '...在路上',
+        'orderNow': '訂購現在',
+        'traditionPassion': '傳統 & 熱情',
+        "aboutUsTitle": '關於我們',
+    "aboutUs": "歡迎來到我們的流動披薩店！這是一個獨特的披薩故事，顧客可以親眼看到他們的披薩在我們特製的披薩自行車上新鮮出爐。<br><br> 我們的故事始於創造一種有趣、互動的方式，讓顧客享受披薩。我們想擺脫傳統的實體披薩店，而是把我們的披薩帶到街上。有了我們特製的自行車烤箱，我們能夠到不同的地點，為饑餓的顧客提供熱騰騰、美味的披薩。 <br><br> 我們只使用最新鮮的食材來製作披薩。我們的麵團是從頭開始做的，我們只使用最好的蕃茄、乳酪和配料來創造完美的披薩。我們以使用當地食材為榮，這不僅支持我們的社區，還確保我們的披薩盡可能地新鮮和美味。<br><br> 在我們的披薩店中，我們認為美食應該是人人都能享用的。這就是為什麼我們提供各種口味的披薩選擇，以適應不同的口味。無論你是肉食愛好者還是素食主義者，我們都有適合你的披薩。 我們以提供優質的客戶服務為榮，確保每一口披薩都讓顧客滿意。 我們致力於對社區產生積極的影響。除了在自行車上準備和烤披薩外，我們還在本地採購食材，並盡可能使用環保包裝和實行可持續方法，以減少對環境的影響。<br><br> 感謝您選擇我們的自行車披薩店。我們期待為您提供最好的披薩，新鮮出爐在我們特製的披薩自行車上！",
+
+    'openingHoursTitle': '營業時間',
+    'openingHours': '每日營業時間為下午2點至晚上10點。',
+'location': '地點',
+    'address': '235新北市中和區和平街36號1 樓',
+        'followUs': '關注我們',
+        'rights': '© 2023 歐利披薩 保留所有權利。'
+
+},
+
+'english': {
+    "home": "Home",
+    'english': "English",
+    "chinese": "Chinese",
+    'onTheMove': '...on the Move',
+    'orderNow': 'Order Now',
+    'traditionPassion': 'Tradition & Passion',
+    "aboutUsTitle": 'About Us',
+    "aboutUs": "Welcome to the pizza shop on wheels! Experience a unique pizza story where our customers can watch their pizzas being freshly baked right in front of their eyes on our custom-built Pizza bicycle. <br><br> Our story began with a desire to create a fun and interactive way for customers to enjoy pizza. We wanted to break away from the traditional brick-and-mortar pizza shops and instead, bring our pizza to the streets. With our custom-built bicycle oven, we are able to travel to different locations and serve up hot, delicious pizzas to hungry customers. <br><br> We use only the freshest ingredients to make our pizzas. Our dough is made from scratch and we use only the finest tomatoes, cheeses, and toppings to create the perfect pizza. We take pride in using locally-sourced ingredients whenever possible, which not only supports our community but also ensures that our pizzas are as fresh and delicious as possible. <br><br> At our pizza shop on the bicycle, we believe that great food should be accessible to everyone. That's why we offer a range of pizza options to suit every taste. Whether you're a meat lover, or vegetarian, we have a pizza that's perfect for you. We take pride in providing excellent customer service and making sure that our customers are satisfied with every bite of their pizza. We are committed to making a positive impact in our community. In addition to preparing and baking our pizzas on bicycle we also source our ingredients locally, we also strive to minimise our environmental impact by using eco-friendly packaging and practising sustainable methods whenever possible. <br><br> Thank you for choosing our pizza shop on the bicycle. We look forward to serving you and bringing you the best pizza you've ever tasted, baked fresh on our custom-built Pizza bicycle!",
+    
+    'openingHoursTitle': 'Opening Hours',
+    'openingHours': 'Open daily from 14pm - 22pm',
+    'location': 'Location',
+    'address': '     Olli Pizza Bar <br> 235, New Taipei City, Zhonghe District, Heping St, 36號1 樓',
+    'followUs': 'Follow Us',
+'rights': '© 2023 Olli Pizza All rights reserved.',
+
+}
+}
+
